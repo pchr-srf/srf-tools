a134e30b6747311b7cb7db479a1ba98b5fa0a727
Philipp Christen <philipp.christen@srf.ch>
2018-02-15 09:04:56 +0100

    working link generator for RA and WV

---
+++ srf-tools.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let contentIdInput = document.getElementById('contentIdInput');
+    let urls = document.querySelectorAll(".links a");
+
+    let fillInContentId = (data) => {
+        contentIdInput.value = data.id;
+
+        urls.forEach((element, index) => {
+            let href = element.dataset.href;
+            element.href = href.replace("$ID", data.id).replace("$URL", data.url);
+        });
+    };
+
+    chrome.tabs.executeScript({
+        code: 'var data = {id: document.querySelector("[data-content-id]").dataset.contentId, url: window.location.origin}; data'
+    }, (results) => {
+        if (results.length > 0 && results[0]) {
+            let returnObject = results[0];
+            fillInContentId(returnObject);
+        }
+    });
+});
