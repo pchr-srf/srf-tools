0b64ebbb40a2e69ff4341d206c4a057534dbf978
Philipp Christen <philpp.christen@srf.ch>
2021-03-25 14:39:08 +0100

    update to v3, working phase banner!

---
+++ scripts/banner.js
@@ -0,0 +1,54 @@
+const getPhase = () => {
+  const url = window.location.href;
+  if(url.indexOf('www-test.') >= 0) {
+    return 'TEST';
+  } else if (url.indexOf('www-stage.') >= 0) {
+    return 'STAGE';
+  } else if (url.indexOf('dev.') >= 0 || url.indexOf('pascal.') >= 0) {
+    return 'DEV';
+  } else {
+    return 'PROD';
+  }
+};
+
+const addBanner = () => {
+  const banner = document.createElement("div"); 
+  document.body.appendChild(banner); 
+  banner.innerText = getPhase();
+  banner.className = 'srf-tools__banner';
+};
+
+const removeBanner = () => {
+  const banner = document.getElementsByClassName('srf-tools__banner');
+  if (banner.length > 0) {
+    banner[0].remove();
+  }
+};
+
+const showEnvironmentBadge = () => {
+  chrome.storage.sync.get("shouldShowEnvironment", ({ shouldShowEnvironment }) => {
+    // prevent duplicating the banner by removing it first, then adding it if the
+    // settings demand it
+    removeBanner();
+
+    if (shouldShowEnvironment) {
+      addBanner();
+    }
+  });
+}
+
+// do the dance on page load
+showEnvironmentBadge();
+
+// do the thing on hash/page change
+window.onhashchange = function() { 
+	showEnvironmentBadge();
+}
+
+// do the thing when receiving a message from the extension
+chrome.runtime.onMessage.addListener(
+  function(request) {
+    if (request.action == "showEnvironmentBadge")
+      showEnvironmentBadge();
+  }
+);
--- srf-tools.js
+++ srf-tools.js
@@ -1,33 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let fillInContentId = (data) => {
-        // Put location in input
-        document.getElementById('contentIdInput').value = data.locationId;
+let showEnvironmentCheckbox = document.getElementById("showEnvironment");
 
-        // create URLs for article variations
-        document.querySelectorAll(".link--replace-url").forEach((element, index) => {
-            let href = element.dataset.href;
-            element.href = href.replace("$ID", data.locationId).replace("$URL", data.origin);
-        });
+chrome.storage.sync.get("shouldShowEnvironment", ({ shouldShowEnvironment }) => {
+  showEnvironmentCheckbox.checked = !!shouldShowEnvironment;
+});
 
-        document.querySelector(".link--old-url").href = data.url + "?wayback=1";
+// When the checkbox is changed, save the setting and let the content scripbt know
+showEnvironmentCheckbox.addEventListener("click", async () => {
+  const shouldShowEnvironment = showEnvironmentCheckbox.checked;
 
-    };
+  // save the setting
+  chrome.storage.sync.set({ shouldShowEnvironment: shouldShowEnvironment });
 
-    chrome.tabs.executeScript({
-        code: `
-        var data = {
-            locationId: document.querySelector("[data-content-id]").dataset.contentId,
-            origin: window.location.origin,
-            url: window.location.href
-        };
-        data`
-    }, (results) => {
-        if (results.length > 0 && results[0]) {
-            let returnObject = results[0];
-            fillInContentId(returnObject);
-        } else {
-            // Article links can only be generated on a srf page
-            document.querySelector("#section--article").style.display = "none";
-        }
+  // send a message to the current tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      action: 'showEnvironmentBadge'
     });
+  });
 });
